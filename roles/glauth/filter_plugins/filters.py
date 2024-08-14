e712b9bec5398f0ea8ca825de9eaca5699ea4875
Dato Simó <dato@net.com.org.es>
2024-08-14 20:37:13 -0300

    Add tests for glauth role

--- roles/glauth/filter_plugins/filters.py
+++ roles/glauth/filter_plugins/filters.py
@@ -3,14 +3,21 @@
 import json
 import toml
 
+# FIXME: should move this to a top-level collection plugin.
+
 
 class FilterModule:
 
     def filters(self):
         return {
             "to_toml": self.to_toml,
+            "from_toml": self.from_toml,
         }
 
     def to_toml(self, variable):
         serialized_json = json.dumps(dict(variable))
         return toml.dumps(json.loads(serialized_json))
+
+    def from_toml(self, data):
+        toml_data = toml.loads(data)
+        return json.loads(json.dumps(dict(toml_data)))
